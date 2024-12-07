82a5331f3e94834ae2171a28f44978cdfe11e35d
david-parkk <ji0513ji@naver.com>
2024-12-07 14:08:17 +0900

    feat: 기능 구현(사원을 관리한다.)
    - 사원은 이름이 있다.
    - 중복된 이름을 가진 사원은 예외로 간주한다.

---
+++ src/main/java/oncall/Member.java
@@ -0,0 +1,33 @@
+package oncall;
+
+import java.util.Objects;
+
+public class Member {
+
+    private String name;
+
+    public Member(String name) {
+        this.name = name;
+    }
+
+    public boolean equals(String name) {
+        return this.name.equals(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Member member = (Member) o;
+        return Objects.equals(name, member.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
---
+++ src/main/java/oncall/MemberFactory.java
@@ -0,0 +1,31 @@
+package oncall;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberFactory {
+
+    private List<Member> members = new ArrayList<>();
+
+    public MemberFactory() {
+
+    }
+
+    public void addMember(Member newMember) {
+        for (Member member : members) {
+            if (newMember.equals(member)) {
+                throw new IllegalArgumentException("이미 존재하는 근무자 입니다");
+            }
+        }
+        members.add(newMember);
+    }
+    
+    public Member getMember(String name) {
+        for (Member member : members) {
+            if (member.equals(name)) {
+                return member;
+            }
+        }
+        throw new IllegalArgumentException("존재하지 않는 근무자 입니다");
+    }
+}
