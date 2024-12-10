ca9a0b6131efa07b1ac1d521be0bbd10d0e322ff
minSsan <msp1125@hanyang.ac.kr>
2024-12-10 13:33:39 +0900

    init: 기본 MVC 구조 클래스 정의

---
+++ src/main/java/oncall/controller/Controller.java
@@ -0,0 +1,35 @@
+package oncall.controller;
+
+import oncall.view.InputView;
+import oncall.view.OutputView;
+
+import java.util.function.Supplier;
+
+public class Controller {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public Controller(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        String value = get();
+    }
+
+    private String get() {
+        return doLoop(inputView::getValue);
+    }
+
+    public <T> T doLoop(Supplier<T> function) {
+        while (true) {
+            try {
+                return function.get();
+            } catch (IllegalArgumentException e) {
+                outputView.printMessage(e.getMessage());
+            }
+        }
+    }
+}
+
---
+++ src/main/java/oncall/view/InputView.java
@@ -0,0 +1,9 @@
+package oncall.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String getValue() {
+        return Console.readLine().trim();
+    }
+}
