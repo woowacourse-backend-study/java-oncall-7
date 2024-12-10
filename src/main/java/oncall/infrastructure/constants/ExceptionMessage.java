6b7aeb1cd8f90e0032a6c606e8b5dd9e2a3e63d3
minSsan <msp1125@hanyang.ac.kr>
2024-12-10 14:06:11 +0900

    feat(Date): 날짜 관련 정보 enum 클래스

--- src/main/java/oncall/infrastructure/constants/ExceptionMessage.java
+++ src/main/java/oncall/infrastructure/constants/ExceptionMessage.java
@@ -5,6 +5,7 @@ import oncall.domain.Worker;
 public enum ExceptionMessage {
     EMPTY_NAME("근무자의 이름은 공백으로 설정할 수 없습니다"),
     EXCEED_NAME_LEN(String.format("근무자의 이름은 최대 %,d 글자까지 설정할 수 있습니다.", Worker.MAX_LEN)),
+    INVALID_DATE("날짜는 1월부터 12월까지 존재합니다."),
     ;
     private final String message;
 
